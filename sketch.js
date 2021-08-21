9b525cf8e2684723450084381e9bfebed9661fdd
ambshobby <70990274+ambshobby@users.noreply.github.com>
2021-08-21 14:28:24 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+var marks=[50,30,99,100];
+var sum=0;
+function setup() 
+{
+  sum=marks[0]+marks[1]+marks[2]+marks[3];
+  console.log("total marks=",sum);
+  createCanvas(400,400);
+}
+
+function draw() 
+{
+background(51);
+
+}
+
